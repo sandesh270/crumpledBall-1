304e8aee12aba18f6009fbceb98a844200c5f8c5
sandesh270 <73105305+sandesh270@users.noreply.github.com>
2020-12-25 09:25:51 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,22 @@
+class Ball {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      // this.body.position.x = mouseX;
+      // this.body.position.y = mouseY;
+      
+      ellipseMode(CENTER);
+      fill("brown");
+      ellipse(this.body.position.x,this.body.position.y, this.width, this.height);
+    }
+  }
---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
+var engine, world;
+var box1, pig1;
+var backgroundImg,platform;
+var constrainedLog;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(600,height-50,1200,20);
+    leftB = new Box(910,height-130,20,150);
+    bottomB = new Box(1090,height-130,20,150);
+    rightB = new Box(1000,height-60,200,20);
+    ball = new Ball(200,150,50,50);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    ground.display();
+    leftB.display();
+    bottomB.display();
+    rightB.display();
+    ball.display();
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW){
+        Matter.Body.applyForce(ball.body, ball.body.position,{x:170,y:-150})
+    }
+}
